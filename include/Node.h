7ba6a22eb8a3441a4b3e1d1698f4d9b49649d375
Tifani Warnita <tiffayumuyuka@gmail.com>
2016-06-21 10:54:37 +0900

    Edit Node class (add pointers and edit ctor dtor)

--- include/Node.h
+++ include/Node.h
@@ -1,36 +1,60 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <string>
+
 
 class Node {
 public:
+    //! CONSTRUCTOR AND DESTRUCTOR
     /** Default constructor */
     Node();
+    /** Constructor with parameter */
+    Node(unsigned int _distance, std::string _word);
     /** Default destructor */
-    ~Node();
+    virtual ~Node();
 
+    //! GETTER AND SETTER
     /** Access distance
      * \return The current value of distance
      */
     unsigned int getDistance() { return distance; }
     /** Set distance
-     * \param val New value to set
+     * \param _distance New value to set
      */
-    void setDistance(unsigned int val) { distance = val; }
+    void setDistance(unsigned int _distance) { distance = _distance; }
     /** Access word
      * \return The current value of word
      */
-    string getWord() { return word; }
+    std::string getWord() { return word; }
     /** Set word
-     * \param val New value to set
+     * \param _word New value to set
      */
-    void setWord(string val) { word = val; }
+    void setWord(std::string _word) { word = _word; }
+    /** Access left node
+     * \return The current value of left node
+     */
+    Node *getLeft() { return left; }
+    /** Set left node
+     * \param _left New value to set
+     */
+    void setLeft(Node *_left) { left = _left; }
+    /** Access right node
+     * \return The current value of right node
+     */
+    Node *getRight() { return right; }
+    /** Set right node
+     * \param _right New value to set
+     */
+    void setRight(Node *_right) { right = _right; }
 
 protected:
 
 private:
-    unsigned int distance; //!< Member variable "distance"
-    string word; //!< Member variable "word"
+    unsigned int distance; //!< Distance from leaf
+    std::string word; //!< Node representation in std::string
+    Node *left; //!< Pointer to left node
+    Node *right; //!< Pointer to right node
 };
 
 #endif // NODE_H
--- src/Node.cpp
+++ src/Node.cpp
@@ -1,11 +1,22 @@
-#include "Node.h"
+#include "../include/Node.h"
 
 Node::Node()
 {
     //ctor
+    left = NULL;
+    right = NULL;
+}
+
+Node::Node(unsigned int _distance, std::string _word) {
+    distance = _distance;
+    word = _word;
+    left = NULL;
+    right = NULL;
 }
 
 Node::~Node()
 {
     //dtor
+    delete left;
+    delete right;
 }
